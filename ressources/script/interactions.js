43b9510d28c163308bfa80adccdc205989588777
LordDarkSider <98459317+LordDarkSider@users.noreply.github.com>
2023-04-01 12:53:43 +0200

    Add files via upload

--- ressources/script/interactions.js
+++ ressources/script/interactions.js
@@ -4,7 +4,19 @@ function go_to(link){
 }
 
 function easter_egg(link){
-    if(confirm("Souhaitez-vous télécharger l'Easter egg ?\n\nℹ️ Si vous êtes sur la version hors ligne cette opération n'est pas nécessaire. Le fichier se trouve déjà dans le dossier 'easter eggs'.")){
+    if(confirm("Souhaitez-vous télécharger l'easter egg ?\n\nℹ️ Si vous êtes sur la version hors ligne cette opération n'est pas nécessaire. Le fichier se trouve déjà dans le dossier 'easter eggs'.")){
         document.location.href = link;
     }
+}
+
+function correction(values){
+    for (var i = 0; i < fields.length; i++) {
+        fields[i].value = values[i];
+    }
+}
+
+//Afficher correction (lien)
+if(location.search.substring(1,12)=="correction=") {
+    correction(location.search.substring(12).split(','));
+    change_key();
 }
